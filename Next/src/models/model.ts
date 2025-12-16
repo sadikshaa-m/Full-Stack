2d53fd2da1b35b76f76ce2a33671ed9fbdc5daa4
sadiksha <sadikshamahat123@gmail.com>
2025-12-16 21:38:00 +0545

    changes

--- Next/src/models/model.ts
+++ Next/src/models/model.ts
@@ -1,5 +1,5 @@
 
-export interface employeeInterface{
+export interface Employee{
     name: string;
     position: string;
     age: number;
@@ -10,6 +10,13 @@ export interface Comment {
     postId: number;
     name: string;
     email: string;
+    body: string;
+}
+
+export interface Post {
+    userId: number;
+    id: number;
+    title: string;
     body: string;
 }
 
