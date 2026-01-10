28f0f470a93e6794ff7b18caf18b8efec10bb7e9
Sadiksha Mahat <sadikshamahat123@gmail.com>
2026-01-10 11:55:28 -0800

    update

--- new frontend/vite-project/src/features/home/Home.jsx
+++ new frontend/vite-project/src/features/home/Home.jsx
@@ -72,10 +72,6 @@ export default function Home() {
       }
     </Formik>
 
-
-
-
-
 <div className='grid grid-cols-2 lg:grid-cols-4 gap-6 mt-6 items-start'>
   {data.products && data.products.length > 0 ? (
     <>
